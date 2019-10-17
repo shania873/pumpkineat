15216221c3adf4272feffea3448f2a12b9a0c347
Caroline van Aerschot <vanaeca@hotmail.com>
2019-10-17 20:33:16 +0200

    commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * pumpkin
+ * 
+ * 
+ * @author Caroline van Aerschot
+ * @version 1.0.5
+ * Copyright 2019. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+  });
+
+})(jQuery, window, document);
